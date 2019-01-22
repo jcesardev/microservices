c9a153e9d9ed87f91871dfdb8badf2d0a6647917
Julio Cesar Bolaños Palacios <julio.bolanos@varangard.com>
2019-01-21 20:36:17 -0600

    Todo MS router added.

--- vetta-api-gateway/routers/router.js
+++ vetta-api-gateway/routers/router.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router()
 var helloService = require('./helloService')
-
+var todoService = require('./todoService')
 router.use((req, res, next) => {
     console.log("Called: ", req.path)
     next()
 })
 
 router.use(helloService)
+router.use(todoService)
 
 module.exports = router
--- vetta-api-gateway/routers/todoService.js
+++ vetta-api-gateway/routers/todoService.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router()
 const apiAdapter = require('./apiAdapter')
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:5000'
 const api = apiAdapter(BASE_URL)
 
-router.get('/hello-api/v1/hello', (req, res) => {
+router.get('/todo-api/v1/tasks', (req, res) => {
   console.log (req.path + " called")
   api.get(req.path).then(resp => {
     res.send(resp.data)
