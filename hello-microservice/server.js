246fec14e92de1e2637c5ef602a4ffeb02ccf526
Julio Cesar Bolaños Palacios <julio.bolanos@varangard.com>
2019-01-21 20:35:23 -0600

    Health Check, added.

--- hello-microservice/server.js
+++ hello-microservice/server.js
@@ -2,9 +2,14 @@ var moment = require('moment');
 var express = require('express');
 var app = express();
 
-app.get('/hello-api/', function (req, res) {
+app.get('/hello-api/v1/hello', function (req, res) {
   console.log('Request Received: ' + moment().format('YYYY-MM-DD hh:mm:ss'))
   res.send('Hello World!');
+});
+
+app.get('/hello-api/healthCheck', function (req, res) {
+  console.log('Request for Health Check ')
+  res.send('OK from Hello API');
 });
 
 app.listen(3000, function () {
